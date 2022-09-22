38f37d329c1184d67d7455ab2098aea6241326ec
Marcia Agostinho <marciaagostinho27@gmail.com>
2022-09-22 08:46:31 -0300

    first commit

---
+++ src/styles/global.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    *{
+        margin:0;
+        padding: 0;
+        
+    }
+    body{
+        font-family: 'Poppins', sans-serif;
+        background-color: #ccc;
+    }
+
+`
+export default GlobalStyles
